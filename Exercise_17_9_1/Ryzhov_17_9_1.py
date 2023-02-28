3540f5d1fab9a871425836407be5a31b571836b2
Alexander <rayen177@gmail.com>
2023-03-01 00:50:16 +0300

    решение для задания 17.9.1 latest

--- Exercise_17_9_1/Ryzhov_17_9_1.py
+++ Exercise_17_9_1/Ryzhov_17_9_1.py
@@ -1,5 +1,3 @@
-string_num = '32 5 9 26 4 5 8 10 3 56'
-
 def sorted_list(array):
     for i in range(len(array)):
         for j in range(len(array) - i - 1):
@@ -17,30 +15,33 @@ def binary_search(array, element, left=0, right=None):
             else:
                 return f'Не возможно установить номер позиции элемента, который меньше введенного числа {element}, а следующий за ним больше или равен числу {element}.\nТ.к. введеное число {element} больше максимального числа {array[right-1]} из заданого списка.'
 
-
-    middle = (right + left) // 2
-    if array[middle] == element:
-
-
-        for index in array:
-            if element in arr
-        return middle - 1
-    elif element < array[middle]:
-        return binary_search(array, element, left, middle - 1)
+    if element in array:
+        middle = (right + left) // 2
+        if array[middle] == element:
+            return middle - 1
+        elif element < array[middle]:
+            return binary_search(array, element, left, middle - 1)
+        else:
+            return binary_search(array, element, middle + 1, right)
     else:
-        return binary_search(array, element, middle + 1, right)
+        for i in array:
 
 
 try:
+    # list_num = [int(num) for num in input('Введите список чисел через пробел: ').split()]
+    string_num = '32 5 9 26 4 5 8 10 3 56'
+    list_num = list(map(int, string_num.split()))
+    print(f'Неотсортированный список чисел: {list_num}')
     per_value = int(input('Введите любое число: '))
 except ValueError:
-    print('Введено неправильное значение')
+    print('Введенное значение не является числом')
 
 else:
-    list_num = list(map(int, string_num.split()))
+
 
     sorted_list_num = sorted_list(list_num)
-    print(sorted_list_num)
+    print(f'Отсортированный список чисел: {sorted_list_num}')
+
     index_per_value = binary_search(list_num, per_value)
     print(index_per_value)
 
